5aa13aefc7f24ec9c4f518917306622e8364e1ee
schneidh <schneidh@a07e0902-2f26-0410-9814-cd25c891c7af>
2007-01-06 06:17:31 +0000

    added support for flow exec key as a parameter
    git-svn-id: http://struts2webflow.googlecode.com/svn/trunk@17 a07e0902-2f26-0410-9814-cd25c891c7af

--- plugin/src/main/java/com/googlecode/struts2webflow/AbstractFlowScopeInterceptor.java
+++ plugin/src/main/java/com/googlecode/struts2webflow/AbstractFlowScopeInterceptor.java
@@ -3,15 +3,25 @@ package com.googlecode.struts2webflow;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.ServletActionContext;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.webflow.context.ExternalContext;
+import org.springframework.webflow.context.ExternalContextHolder;
+import org.springframework.webflow.core.collection.MutableAttributeMap;
+import org.springframework.webflow.execution.FlowExecution;
 import org.springframework.webflow.execution.RequestContext;
+import org.springframework.webflow.execution.repository.FlowExecutionKey;
+import org.springframework.webflow.execution.repository.FlowExecutionRepository;
 import org.springframework.webflow.executor.FlowExecutor;
+import org.springframework.webflow.executor.FlowExecutorImpl;
 
 import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.config.ConfigurationException;
 import com.opensymphony.xwork2.interceptor.Interceptor;
 import com.opensymphony.xwork2.interceptor.PreResultListener;
@@ -21,6 +31,11 @@ import com.opensymphony.xwork2.interceptor.PreResultListener;
  */
 public abstract class AbstractFlowScopeInterceptor implements Interceptor,
 		PreResultListener {
+	/**
+	 * Parameter name for the flow execution key. (If submitted as a parameter)
+	 */
+	public static final String FLOW_EXECUTION_KEY = "flowExecutionKey";
+
 	/**
 	 * The spring id of the flow executor bean for this action. If this is not
 	 * set, it is assumed that the flow executor will be configured with a
@@ -33,6 +48,11 @@ public abstract class AbstractFlowScopeInterceptor implements Interceptor,
 	 * events in flow executions.
 	 */
 	private FlowExecutor flowExecutor;
+
+	/**
+	 * Location of the flow exec key if stored in session.
+	 */
+	private String sessionKey = SessionFlowExecKeyInterceptor.DEFAULT_SESSION_KEY;
 
 	/**
 	 * @return true if we are are executing within the workflow.
@@ -59,12 +79,45 @@ public abstract class AbstractFlowScopeInterceptor implements Interceptor,
 		}
 	}
 
+	/**
+	 * Helper method to access flow scope from outside the flow.
+	 */
+	protected Map getFlowScopeExternal(FlowExecutor flowExecutor) {
+		FlowExecutionRepository repo = ((FlowExecutorImpl) flowExecutor)
+				.getExecutionRepository();
+		final ActionContext actionContext = ActionContext.getContext();
+		final ActionInvocation actionInvocation = actionContext
+				.getActionInvocation();
+		final ServletContext servletContext = ServletActionContext
+				.getServletContext();
+		final HttpServletRequest request = ServletActionContext.getRequest();
+		final HttpServletResponse response = ServletActionContext.getResponse();
+
+		ExternalContext context = new Struts2ExternalContext(actionInvocation,
+				servletContext, request, response);
+		ExternalContextHolder.setExternalContext(context);
+
+		// first check for flow exec key as parameter
+		String flowExecKey = (String) actionInvocation.getInvocationContext()
+				.getParameters().get(FLOW_EXECUTION_KEY);
+		if (flowExecKey == null) {
+			// second look in the session
+			Map sessionMap = actionContext.getSession();
+			flowExecKey = (String) sessionMap.get(sessionKey);
+		}
+		FlowExecutionKey key = repo.parseFlowExecutionKey(flowExecKey);
+		FlowExecution flowExecution = repo.getFlowExecution(key);
+		MutableAttributeMap attrMap = flowExecution.getActiveSession()
+				.getScope();
+		return attrMap.asMap();
+	}
+
 	public void init() {
 	}
 
 	public void destroy() {
 	}
-	
+
 	/**
 	 * Returns the flow executor used by this interceptor.
 	 * 
@@ -110,5 +163,13 @@ public abstract class AbstractFlowScopeInterceptor implements Interceptor,
 	 */
 	public void setFlowExecutorBean(String flowExecutorBean) {
 		this.flowExecutorBean = flowExecutorBean;
+	}
+
+	public String getSessionKey() {
+		return sessionKey;
+	}
+
+	public void setSessionKey(String sessionKey) {
+		this.sessionKey = sessionKey;
 	}
 }
