3c8b89bec179a184c2dada88b0683f12d27575a5
schneidh <schneidh@a07e0902-2f26-0410-9814-cd25c891c7af>
2007-05-15 16:05:36 +0000

    Issue 6: Patch to allow the action name, namespace and method to be specified in the webflow configuration
    git-svn-id: http://struts2webflow.googlecode.com/svn/trunk@40 a07e0902-2f26-0410-9814-cd25c891c7af

--- plugin/src/main/java/com/googlecode/struts2webflow/Struts2FlowAdapter.java
+++ plugin/src/main/java/com/googlecode/struts2webflow/Struts2FlowAdapter.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.struts2.dispatcher.Dispatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.webflow.action.AbstractAction;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
@@ -30,27 +31,52 @@ public class Struts2FlowAdapter extends AbstractAction {
     public static final String REQUEST_CONTEXT = "com.opensymphony.xwork.ActionContext.context";
 
     /**
+     * Adapt attributes
+     * @{
+     */
+    public static final String ATTRIBUTE_ADAPT_ACTION = "action";
+    public static final String ATTRIBUTE_ADAPT_METHOD = "method";
+    public static final String ATTRIBUTE_ADAPT_NAMESPACE = "namespace";
+
+    /** @} */
+
+    /**
      * @see org.springframework.webflow.action.AbstractAction#doExecute(org.springframework.webflow.RequestContext)
      */
+    @SuppressWarnings("unchecked")
     protected Event doExecute(RequestContext context) throws Exception {
         Struts2ExternalContext webworkContext = (Struts2ExternalContext) context
             .getExternalContext();
         ActionInvocation invocation = webworkContext.getActionInvocation();
         ActionProxy proxy = invocation.getProxy();
 
-        // Assume the current state id is the name of the webwork action defined
-        // in the xwork.xml
-        String actionName = context.getCurrentState().getId();
+        // Check attributes for the name of the adapted action
+        String actionName = (String) context.getAttributes().get(
+            ATTRIBUTE_ADAPT_ACTION);
+        if(!StringUtils.hasText(actionName)) {
+            // Assume the current state id is the name of the webwork action defined
+            // in the xwork.xml
+            actionName = context.getCurrentState().getId();
+        }
+
+        // Assume the method to call
+        String method = (String) context.getAttributes().get(
+            ATTRIBUTE_ADAPT_METHOD);
 
         // Assume the current namespace
-        String namespace = invocation.getProxy().getNamespace();
+        String namespace = (String) context.getAttributes().get(
+            ATTRIBUTE_ADAPT_NAMESPACE);
+        if(!StringUtils.hasText(namespace)) {
+            namespace = invocation.getProxy().getNamespace();
+        }
 
         ValueStack stack = ActionContext.getContext().getValueStack();
         String finalNamespace = TextParseUtil.translateVariables(namespace,
             stack);
         String finalActionName = TextParseUtil.translateVariables(actionName,
             stack);
-        String finalMethodName = null;
+        String finalMethodName = StringUtils.hasText(method) ? TextParseUtil
+            .translateVariables(method, stack) : null;
 
         HashMap extraContext = new HashMap();
         extraContext.put(ActionContext.VALUE_STACK, ActionContext
@@ -67,8 +93,8 @@ public class Struts2FlowAdapter extends AbstractAction {
 
         Configuration config = du.getConfigurationManager()
             .getConfiguration();
-        ActionProxyFactory factory = (ActionProxyFactory) config
-            .getContainer().getInstance(ActionProxyFactory.class);
+        ActionProxyFactory factory = config.getContainer().getInstance(
+            ActionProxyFactory.class);
         proxy = factory.createActionProxy(finalNamespace, finalActionName,
             extraContext, false, true);
         if(null != finalMethodName) {
